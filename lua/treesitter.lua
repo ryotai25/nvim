8d2f0ee6cf1ff7483509ddddae75a98131c7160a
Ryotai Yamada <ryotai1025@gmail.com>
2022-09-02 22:02:19 +0900

    add treesitter install_lang

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,4 +1,5 @@
 require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "julia", "lua", "vim", "latex", "markdown", "json", "fish", "python", "r", "toml"},
   highlight = {
     enable = true,
   }
