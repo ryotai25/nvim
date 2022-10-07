772c5793187fb81505863512d9f88e8358ec5e4b
Ryotai Yamada <ryotai1025@gmail.com>
2022-10-07 16:28:49 +0900

    update files

--- lua/iron.lua
+++ lua/iron.lua
@@ -2,35 +2,26 @@ local iron = require("iron.core")
 
 iron.setup {
   config = {
-    -- If iron should expose `<plug>(...)` mappings for the plugins
-    should_map_plug = false,
-    -- Whether a repl should be discarded or not
-    scratch_repl = true,
-    -- Your repl definitions come here
-    repl_definition = {
+    should_map_plug = false, -- If iron should expose `<plug>(...)` mappings for the plugins
+    scratch_repl = true, -- Whether a repl should be discarded or not
+    repl_definition = { -- Your repl definitions come here
       sh = {
-        command = {"zsh"}
+        command = {"fish"}
       }
     },
     -- repl_open_cmd = require('iron.view').curry.bottom(40),
     repl_open_cmd = 'belowright 72 vsplit',
-    -- how the REPL window will be opened, the default is opening
-    -- a float window of height 40 at the bottom.
   },
-  -- Iron doesn't set keymaps by default anymore. Set them here
-  -- or use `should_map_plug = true` and map from you vim files
   keymaps = {
-    send_motion = "<leader>sc",
-    visual_send = "<leader>sc",
-    send_file = "<leader>sf",
-    send_line = "<leader>ss",
-    cr = "<leader>s<cr>",
-    interrupt = "<leader>s<space>",
-    exit = "<leader>sq",
-    clear = "<leader>cl",
+    send_motion = "<Leader>sc",
+    visual_send = "<Leader>sc",
+    send_file = "<Leader>sf",
+    send_line = "<Leader>ss",
+    cr = "<Leader>s<cr>",
+    interrupt = "<Leader>s<space>",
+    exit = "<Leader>sq",
+    clear = "<Leader>cl",
   },
-  -- If the highlight is on, you can change how it looks
-  -- For the available options, check nvim_set_hl
   highlight = {
     italic = true
   }
