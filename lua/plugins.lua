0b9599bc324aa7dc387afaeab44f9136e1f13577
Ryotai Yamada <ryotai1025@gmail.com>
2022-11-06 04:28:41 +0900

    rm packer_compiled.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,25 +20,7 @@ require'packer'.startup(function(use)
       { 'nvim-treesitter/nvim-treesitter-textobjects' },
     },
     run = ':TSUpdate',
-    config = function()
-      require'nvim-treesitter.configs'.setup {
-        ensure_installed = { "julia", "lua", "vim", "latex", "markdown", "json", "fish", "python", "r", "toml", "bash", "regex"},
-        highlight = { enable = true },
-        textobjects = {
-          select = {
-            enable = true,
-            loadahead = true,
-
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["al"] = "@loop.outer",
-              ["il"] = "@loop.inner",
-            }
-          }
-        }
-      }
-    end,
+    config = get_setup("nvim-treesitter"),
   }
 
   use {
