90df5e2808e4ab6235b3a8b51863ffe93368cfb9
Ryotai Yamada <ryotai1025@gmail.com>
2022-12-27 02:09:07 +0900

    packer.nvim -> lazy.nvim first commit

--- init.lua
+++ init.lua
@@ -1,3 +1,18 @@
-require('plugins')
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+-- require('plugins')
+require('plugins_lazy')
+
 require('util')
 require('mapping')
